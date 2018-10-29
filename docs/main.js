96244f703cc7815a0e74cc9d3c9b536affd7a2cd
malaJO <jovana.pavic@gmail.com>
2018-10-29 12:27:35 +0100

    Styled and created Resume page

--- docs/main.js
+++ docs/main.js
@@ -17,3 +17,23 @@ function logScroll() {
   var scrollY = window.pageYOffset || document.documentElement.scrollTop;
   console.log(scrollY);
 }
+
+
+function scrollNav() {
+  var scrollY = window.pageYOffset || document.documentElement.scrollTop;
+  var navig = document.getElementsByTagName('nav');
+  var logoJp = document.getElementsByClassName('JPlogo');
+  var projects = document.getElementsByClassName('Projects');
+  if (scrollY>80){
+    navig[0].classList.add("scrollUp");
+    logoJp[0].classList.add("scrollDown");
+    projects[0].classList.add("projRevealed");
+  } else {
+    navig[0].classList.remove("scrollUp");
+    logoJp[0].classList.remove("scrollDown");
+    projects[0].classList.remove("projRevealed");
+  }
+}
+
+
+window.addEventListener('scroll', scrollNav);
